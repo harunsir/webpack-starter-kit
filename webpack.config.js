1705123544f45934716356dd0304ceca76ab7c00
harunsir <tasfee09@gmail.com>
2022-11-29 07:34:40 +0600

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+ entry: "./index.js",
+ module: {
+  rules: [
+   {
+    test: /\.svg$/,
+    use: "svg-inline-loader"
+   },
+   {
+    test: /\.css$/i,
+    use: ["style-loader", "css-loader"],
+   },
+   {
+    test: /\.(js)$/,
+    use: "babel-loader",
+   }
+  ]
+ },
+ output: {
+  path: path.resolve(__dirname, "dist"),
+  filename: "bundle.js",
+ },
+ plugins: [new HtmlWebpackPlugin()],
+ mode: "development",
+};
